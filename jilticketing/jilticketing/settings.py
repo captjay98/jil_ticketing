bcf620afc66c02c6c9a1faadf7029e7a3da7e2b4
captjayWSL <captjay98@gmail.com>
2022-11-16 17:57:43 +0100

    fixed typo in settings and made migrations

--- jilticketing/jilticketing/settings.py
+++ jilticketing/jilticketing/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-AUTH_USER_MODEL = "account.USER"
+AUTH_USER_MODEL = "accounts.USER"
 AUTHENTICATION_BACKEND = (
     "django.contrib.auth.backends.AllowAllUserModelBackend",
     "accounts.backends.CaseInsensitiveModelBackend"
@@ -35,13 +35,14 @@ AUTHENTICATION_BACKEND = (
 # Application definition
 
 INSTALLED_APPS = [
+    "accounts",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "accounts",
+    
 ]
 
 MIDDLEWARE = [
