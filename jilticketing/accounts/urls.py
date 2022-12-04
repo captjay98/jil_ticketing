9e20e6ee3b9eb30dfebbea10cba3eb08d4df30d8
captjayWSL <captjay98@gmail.com>
2022-12-04 01:48:15 +0100

    added the payment view and url path also made some adjustments to the booking view

--- jilticketing/accounts/urls.py
+++ jilticketing/accounts/urls.py
@@ -3,14 +3,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name="home"),
+    path("", views.home, name="home"),
     path("register", views.RegisterView, name="registration"),
     path("login", views.LoginView, name="login"),
-    path("logout",views.LogoutView, name="logout"),
+    path("logout", views.LogoutView, name="logout"),
     path("profile", views.ProfileView, name="profile"),
     path("trip", views.TripView, name="trip"),
     path("trips", views.TripsView, name="trips"),
     path("book", views.BookingView, name="book"),
     path("confirm", views.ConfirmView, name="confirm"),
     path("seat", views.SeatView, name="seat"),
+    path("payment", views.PayView, name="payment"),
 ]
