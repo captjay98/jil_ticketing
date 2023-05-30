733367f1f5c4f29b0cb208ce80884a8b231a05d8
captjayferen <captjay98@gmail.com>
2023-05-30 23:32:44 +0100

    fixed the settings  Please enter the commit message for your changes. Lines starting

--- jilticketing/jilticketing/settings.py
+++ jilticketing/jilticketing/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATIC_ROOT = BASE_DIR / "staticfiles"
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # STATICFILES_DIRS = [
