7fe660953bbf2f501f7cfb31cbd4c925e42396ef
captjayferen <captjay98@gmail.com>
2023-05-30 23:35:32 +0100

    fix

--- jilticketing/jilticketing/settings.py
+++ jilticketing/jilticketing/settings.py
@@ -133,12 +133,14 @@ USE_I18N = True
 USE_TZ = True
 
 
-STATIC_URL = "/static/"
-if not DEBUG:
-    STATIC_ROOT = BASE_DIR / "staticfiles"
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-# STATICFILES_DIRS = [
+STATIC_URL = '/static/'
+# Following settings only make sense on production and may break development environments.
+if not DEBUG:    # Tell Django to copy statics to the `staticfiles` directory
+    # in your application directory on Render.
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    # Turn on WhiteNoise storage backend that takes care of compressing static files
+    # and creating unique names for each version so they can safely be cached forever.
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'# STATICFILES_DIRS = [
 #     BASE_DIR / "static",
 # ]
 # STATIC_ROOT = BASE_DIR / "staticfiles"
